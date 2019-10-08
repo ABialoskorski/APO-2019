96f0b86b30f6ae6e4700a79b447c16d229975f20
Wiktor Bałaban <wiktorbalaban@gmail.com>
2019-10-08 17:58:59 +0200

    Project init

---
+++ Food vending machine prototype/Food vending machine prototype/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Food_vending_machine_prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
