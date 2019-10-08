dbc3e165c00779b709b89db9867e388bafd02349
Artur Białoskórski <whiteskin11@gmail.com>
2019-10-08 18:27:32 +0200

    simple object

---
+++ Prototyp-Artur/Machine.js
@@ -0,0 +1,49 @@
+const Products = [
+    {
+        id: 0,
+        name: "Kitkat",
+        price: 2,
+    },
+    {
+        id: 1,
+        name: "Sok pomarańczowy",
+        price: 1,
+    },
+    {
+        id: 2,
+        name: "Kawa",
+        price: 2,
+    },
+    {
+        id: 3,
+        name: "Ciepły kubek knorr",
+        price: 4,
+    },
+    {
+        id: 4,
+        name: "10 Czekoladek",
+        price: 6,
+    },
+    {
+        id: 5,
+        name: "Coca-Cola",
+        price: 3,
+    },
+];
+let Welcome = () => {
+    console.log('Witaj, wybierz produkt z automatu spożywczego');
+    switch (number) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+    }
+};
+
+Welcome();
