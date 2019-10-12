a1fc17d601b115a4c270830b0e6e8eb8525460cc
Artur Białoskórski <bialoskorski.artur@gmail.com>
2019-10-12 16:39:52 +0200

    added timeout

--- Prototyp-Artur/Machine.js
+++ Prototyp-Artur/Machine.js
@@ -3,7 +3,7 @@ var readline = require("readline");
 var rl = readline.createInterface(process.stdin, process.stdout);
 
 const choosedOne = {
-	id: 15,
+	id: 100,
 	nazwa: "",
 	cena: 0,
 };
@@ -12,13 +12,13 @@ const Products = [
 	{
 		id: 0,
 		nazwa: "Kitkat",
-		cena: 2,
+		cena: 3.2,
 		ilosc: 20,
 	},
 	{
 		id: 1,
 		nazwa: "Sok pomarańczowy",
-		cena: 1,
+		cena: 2,
 		ilosc: 300,
 	},
 	{
@@ -30,7 +30,7 @@ const Products = [
 	{
 		id: 3,
 		nazwa: "Ciepły kubek knorr",
-		cena: 4,
+		cena: 4.6,
 		ilosc: 10,
 	},
 	{
@@ -50,50 +50,57 @@ const Products = [
 let Choose = () => {
 	console.log("Lista dostępnych produktów:");
 	console.log(Products);
-	rl.question("Wybierz produkt po numerze id: ", number => {
-		if (Products[number].ilosc <= 0) {
-			console.log(
-				"Brak zapasów tego produktu, wysłano zgłoszenie do magazynu"
-			);
-			process.exit();
-		}
-		let leftAmount = Products[number].ilosc - 1;
-
-		if (leftAmount <= 0) {
-			console.log(
-				"Twój produkt jest ostatnim tego typu, brawo szczęśliwcu, wysłaliśmy zgłoszenie po kolejne zapasy"
-			);
-		}
-
-		choosedOne.id = number;
-		choosedOne.nazwa = Products[number].nazwa;
-		choosedOne.cena = Products[number].cena;
-
-		console.log(
-			`Wybrano ${choosedOne.nazwa}, cena wynosi ${choosedOne.cena}zł`
-		);
-		if (choosedOne.id == Products[3].id) {
-			console.log(
-				"Wybrano ciepły posiłek, automat przywołuje obsługę serwisową"
-			);
-		}
-
-		rl.question(
-			`Wybierz sposób płatności: karta lub gotówka
-        `,
-			payment => {
-				if (payment == "karta") {
-					console.log(
-						`Wybrano płatność kartą, do zapłaty: ${choosedOne.cena}zł`
-					);
-				} else if (payment == "gotowka") {
-					console.log(
-						`Wybrano płatność gotówką, do zapłaty: ${choosedOne.cena}zł`
-					);
-				}
+	rl.question(
+		`
+    Wybierz produkt po numerze id: `,
+		number => {
+			if (Products[number].ilosc <= 0) {
+				console.log(
+					"Brak zapasów tego produktu, wysłano zgłoszenie do magazynu"
+				);
+				process.exit();
 			}
-		);
-	});
+			let leftAmount = Products[number].ilosc - 1;
+
+			if (leftAmount <= 0) {
+				console.log(
+					"Twój produkt jest ostatnim tego typu, brawo szczęśliwcu, wysłaliśmy zgłoszenie po kolejne zapasy"
+				);
+			}
+
+			choosedOne.id = number;
+			choosedOne.nazwa = Products[number].nazwa;
+			choosedOne.cena = Products[number].cena;
+
+			console.log(
+				`Wybrano ${choosedOne.nazwa}, cena wynosi ${choosedOne.cena}zł`
+			);
+			if (choosedOne.id == Products[3].id) {
+				console.log(
+					"Ponieważ wybrano ciepły posiłek, automat przywołuje obsługę serwisową"
+				);
+			}
+
+			rl.question(
+				`Wybierz sposób płatności: karta lub gotówka
+        `,
+				payment => {
+					if (payment == "karta") {
+						console.log(
+							`Wybrano płatność kartą, do zapłaty: ${choosedOne.cena}zł`
+						);
+					} else if (payment == "gotowka") {
+						console.log(
+							`Wybrano płatność gotówką, do zapłaty: ${choosedOne.cena}zł`
+						);
+					}
+					setTimeout(() => {
+						console.log(`Produkt jest gotowy, do zobaczenia`);
+					}, 2000);
+				}
+			);
+		}
+	);
 };
 
 Choose();
